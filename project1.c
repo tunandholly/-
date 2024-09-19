a5b538a79f69bdb9bdd30d56a6cb38158ed211c6
Herna Li <91788663+tunandholly@users.noreply.github.com>
2024-09-19 23:15:07 +0800

    Update project1.c

--- project1.c
+++ project1.c
@@ -14,6 +14,7 @@ int recordCount = 0;
 
 void createRecord(){
     
+    printf("~create the record~\n");
     records[recordCount].id = recordCount + 1;
     printf("please enter name:\n");
     scanf("%s", records[recordCount].name);
@@ -27,20 +28,24 @@ void createRecord(){
 void readRecord(){
     
     int id_read;
-    printf("read the record\n");
+    printf("~read the record~\n");
     printf("please enter the id you want to read:\n");
     scanf("%d", &id_read);
-    printf("id : %d name : %s age : %d \n", records[id_read-1].id, records[id_read-1].name, records[id_read-1].age);
-
+    if(id_read > 0 && id_read <= recordCount){
+        printf("id : %d name : %s age : %d \n", records[id_read-1].id, records[id_read-1].name, records[id_read-1].age);
+    }else{
+        printf("invalid id!\n");
+    }
 }
 
 void updateRecord(){
 
+    printf("~update the record~\n");
     int id_update;
     
     if(id_update > 0 && id_update <= recordCount){
         
-        printf("enter the record you want to update:\n");
+        printf("enter the record you want to update: \n");
         scanf("%d", &id_update);
         printf("please enter the new name: ");
         scanf("%s", records[id_update-1].name);
@@ -54,9 +59,11 @@ void updateRecord(){
 }
 
 void deleteRecord(){
+
+    printf("~delete the record~\n");
     
     int id_delete;
-    printf("please enter the id you want to delete:");
+    printf("please enter the id you want to delete: ");
     scanf("%d", &id_delete);
 
     if(id_delete > 0 && id_delete <= recordCount){
@@ -78,7 +85,8 @@ int main(){
 
     int choice;
     do{
-        print("\n1. create\n2. read\n3. update\n4. delete\n");
+        printf("\n1. create\n2. read\n3. update\n4. delete\n5. exit\n");
+        printf("please enter the number (1~5) to perform: ");
         scanf("%d", &choice);
 
         switch (choice)
